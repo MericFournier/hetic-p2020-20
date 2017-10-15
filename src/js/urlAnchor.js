f6fb02051d5a5055c9aaeac553da8ba7ad74aa9f
Nicolas Bernaux <bernaux.n@gmail.com>
2017-10-15 02:54:22 +0200

    :bug: Fix bug when anchor was empty

--- src/js/urlAnchor.js
+++ src/js/urlAnchor.js
@@ -1,7 +1,9 @@
 const anchor = () => {
     const hash = window.location.hash.substr(1)
-    const section_to_hach = document.querySelector('section[data-anchor='+hash+']')
-    document.querySelector('#smoothScroll-wrapper').style.transform = 'translateY(-'+ section_to_hach.getAttribute('data-scroll')*100+'vh)'
+    if(hash){
+        const section_to_hach = document.querySelector('section[data-anchor='+hash+']')
+        document.querySelector('#smoothScroll-wrapper').style.transform = 'translateY(-'+ section_to_hach.getAttribute('data-scroll')*100+'vh)'
+    }
 }
 
 
