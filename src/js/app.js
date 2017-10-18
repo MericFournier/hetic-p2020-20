1f471a4ea8bf07f8a3e27b85d27bd41a1d519125
Nicolas Bernaux <bernaux.n@gmail.com>
2017-10-18 21:07:29 +0200

    :hammer: Transform scroll function in object

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
-import {listenScroll }from './smoothScroll';
+import ObjScroll from './smoothScroll';
 
 import anchor from './urlAnchor';
 
 
+var scroll = new ObjScroll()
+scroll.listenScroll()
+
 // scroll function
-anchor()
-listenScroll()
+// anchor()
+// listenScroll()
--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,119 +1,152 @@
-let event_timeStamp     = -1500
-const scroll_wrapper    = document.querySelector('#smoothScroll-wrapper') // translate wrapper
-const scroll_section    = scroll_wrapper.querySelectorAll('section')
-const previous_button   = scroll_wrapper.querySelectorAll('.navigation-button .previous-navigation-button')
-const next_button       = scroll_wrapper.querySelectorAll('.navigation-button .next-navigation-button')
+class ObjScroll {
+    constructor(){
+        this.event_timeStamp    = -1500
+        this.scroll_wrapper     = document.querySelector('#smoothScroll-wrapper') // translate wrapper
+        this.scroll_section     = this.scroll_wrapper.querySelectorAll('section') // get the active section
 
-const listenScroll = () => {
 
-    previous_button.forEach((_previous_button) => {
-        _previous_button.addEventListener('click', (e) => {
-            previousSection()
-            e.preventDefault()
-        })
-    })
-    next_button.forEach((_next_button) => {
-        _next_button.addEventListener('click', (e) => {
-            nextSection()
-            e.preventDefault()
-        })
-    })
+    }
+    listenScroll(){
 
-    document.querySelector('.test').innerHTML = "test"
-
-    if('ontouchstart' in window){
-        touchScreenAnimation()
-        document.querySelector('.test').innerHTML = "j'accepte le touch screen"
-        window.addEventListener('scroll', (e) => {
-            console.log
-            e.preventDefault()
-        })
-        document.ontouchmove = function (e) {
-          e.preventDefault();
+        // Test tactile device
+        if('ontouchstart' in window){
+            this.mobileListener()
         }
+        this.desktopListener()
+        this.buttonListener()
     }
-    else{
-        // Listen scroll (wheel)
+    mobileListener(){
+        // iphone block scroll
+        window.addEventListener('scroll', (e) => {
+            e.preventDefault()
+            window.scroll(0,0)
+        })
+        window.addEventListener('touchmove', (e) => {
+            e.preventDefault()
+        })
+
+        // Homemade scroll listener
+        let touch = {}
+        let direction = {}
+
+        document.addEventListener('touchstart', (e) => {
+            touch.start = parseInt(e.changedTouches[0].pageY)
+        })
+        document.addEventListener('touchend', (e) => {
+            touch.end = parseInt(e.changedTouches[0].pageY)
+            if(touch.start - 5 > touch.end){
+                direction.axe = "y"
+                direction.orientation = parseInt(1)
+                this.scrollAnimation(direction)
+            }
+            else if(touch.start + 5 < touch.end) {
+                direction.axe = "y"
+                direction.orientation = parseInt(-1)
+                this.scrollAnimation(direction)
+            }
+
+        })
+    }
+    desktopListener(){
+        // Wheel (scroll)
         window.addEventListener('wheel', (e) => {
-            scrollAnimation(getWheelDirection(e))
+            this.scrollAnimation(this.getWheelDirection(e))
         })
 
-        // Listen keyboardNavigation
+        // Keyboard
         window.addEventListener('keydown', (e) => {
-            keyboardNavigation(e)
+            this.keyboardNavigation(e)
         })
     }
-}
-
-const getWheelDirection = (e) => {
+    buttonListener(){
+        const previous_button    = this.scroll_wrapper.querySelectorAll('.navigation-button .previous-navigation-button')
+        const next_button        = this.scroll_wrapper.querySelectorAll('.navigation-button .next-navigation-button')
+        // Button previous
+        console.log('button')
+        previous_button.forEach((_previous_button) => {
+            _previous_button.addEventListener('click', (e) => {
+                this.previousSection()
+                e.preventDefault()
+            })
+        })
+        // Button next
+        next_button.forEach((_next_button) => {
+            _next_button.addEventListener('click', (e) => {
+                this.nextSection()
+                e.preventDefault()
+            })
+        })
+    }
+    getWheelDirection(e){
         const deltaX = Math.max(-1, Math.min(1, (e.deltaX) )) // Get scroll X
         const deltaY = Math.max(-1, Math.min(1, (e.deltaY) )) // Get scroll Y
 
-        if(e.timeStamp > (event_timeStamp+1500)){ // Get wheel event only every 1.5 second
-            event_timeStamp = e.timeStamp
+        // Get wheel event only every 1.5 second
+        if(e.timeStamp > (this.event_timeStamp+1500)){
+            this.event_timeStamp = e.timeStamp
             if (deltaY != 0) {
                 if(deltaY > 0){
-                    nextSection()
+                    this.nextSection()
                 }
                 else {
-                    previousSection()
+                    this.previousSection()
                 }
             }
         }
         else {
             return false
         }
-}
-
-const touchScreenAnimation = () => {
-    let touch_start
-    let touch_end
-    let direction = {}
-
-    document.addEventListener('touchstart', (e) => {
-        touch_start = parseInt(e.changedTouches[0].pageY)
-    })
-    document.addEventListener('touchend', (e) => {
-        touch_end = parseInt(e.changedTouches[0].pageY)
-        if(touch_start - 5 > touch_end){
-            direction.axe = "y"
-            direction.orientation = parseInt(1)
-             scrollAnimation(direction)
-        }
-        else if(touch_start + 5 < touch_end) {
-            direction.axe = "y"
-            direction.orientation = parseInt(-1)
-             scrollAnimation(direction)
-        }
-
-    })
-
-}
-
-const nextSection = () => {
-    let direction = {}
-    direction.axe = "y"
-    direction.orientation = parseInt(1)
-    scrollAnimation(direction)
-}
-
-const previousSection = () => {
-    let direction = {}
-    direction.axe = "y"
-    direction.orientation = parseInt(-1)
-    scrollAnimation(direction)
-}
-
-const keyboardNavigation = (e) => {
-    // down
-    if(e.keyCode == 40 ){
-        nextSection()
     }
-    //up
-    else if(e.keyCode == 38 ){
-        previousSection()
+    keyboardNavigation(e){
+        // Down
+        if(e.keyCode == 40 ){
+            this.nextSection()
+        }
+        // Up
+        else if(e.keyCode == 38 ){
+            this.previousSection()
+        }
+    }
+
+    nextSection(){
+        let direction = {}
+        direction.axe = "y"
+        direction.orientation = parseInt(1)
+        this.scrollAnimation(direction)
+    }
+    previousSection(){
+        let direction = {}
+        direction.axe = "y"
+        direction.orientation = parseInt(-1)
+        this.scrollAnimation(direction)
+    }
+
+    scrollAnimation(direction){
+
+        // scroll Y
+        if (direction && direction.axe == 'y'){
+
+            const scrollY_active = this.scroll_wrapper.querySelector('.scrollY-active') // get the active section
+            const active_attribute = parseInt(scrollY_active.getAttribute('data-scroll')) // get active section position
+            const translate_values = active_attribute + direction.orientation // get the next of translate
+
+            if(translate_values < this.scroll_section.length && translate_values >= 0){
+
+                this.scroll_wrapper.style.transform = "translateY(-"+ translate_values*100 +"vh)" // translate action
+
+                //Change the active section
+                scrollY_active.classList.remove("scrollY-active")
+                this.scroll_section[translate_values].classList.add('scrollY-active')
+
+                window.location.hash = '#' + this.scroll_wrapper.querySelector('.scrollY-active').getAttribute('data-anchor') // Anchor in url
+            }
+        }
     }
 }
+
+export default ObjScroll
+
+/*
 
 const scrollAnimation = (direction) => {
 
@@ -141,3 +174,6 @@ export {
     listenScroll,
     scrollAnimation
 }
+
+
+*/
