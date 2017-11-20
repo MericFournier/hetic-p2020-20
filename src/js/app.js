4a71490bc07898478b93534a354fc1e7c73b8667
NicolasBernaux <bernaux.n@gmail.com>
2017-11-20 12:36:42 +0100

    :sparkles: Add drag

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import ScrollListener from './ScrollListener'
 import ScrollAnimation from './ScrollAnimation'
 import Bubble from './Bubble'
 import Anchor from './Anchor'
+import Draggable from './Draggable'
+
 
 window.addEventListener('load', () => {
   // Anchor test
@@ -16,9 +18,22 @@ window.addEventListener('load', () => {
   const scroll = new ScrollListener(scrollInit, 1500, true)
 
 
-  // let detail = new DetailBottle()
-  // detail.buttonListener()
-
   const bubbles = new Bubble()
   bubbles.createBubble()
 })
+
+const element = document.querySelector('.swiper__circle')
+const options = {
+  grid: 200,
+  limit: {
+    x: [1, 200],
+    y: [-5, 5]
+  },
+  smoothDrag: true,
+  onDrag: (drag, e) => {
+    if (e >= 200) {
+      console.log('azertyu')
+    }
+  },
+}
+new Draggable(element, options)
